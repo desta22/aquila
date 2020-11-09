d5ee74c5bbac55bd414522202bc3ba56a1d8ac0b
desta22 <dejanstgm@gmail.com>
2020-11-09 21:27:23 +0100

    footer & header

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 
 ### Advanced WordPress Theme Development
 tutorial from: https://www.youtube.com/watch?v=lNtw4yxEydM&list=PLD8nQCAhR3tT3ehpyOpoYeUj3KHDEVK9h
+
+## STAO KOD
+https://youtu.be/DIyvwx806nw?list=PLD8nQCAhR3tT3ehpyOpoYeUj3KHDEVK9h&t=29
--- index.php
+++ index.php
@@ -1,16 +1,12 @@
-<?php 
+<?php
+
 /**
  * Main Template File
  */
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WP Theme</title>
-</head>
-<body>
-    xxxxxxxx
-</body>
-</html>
+
+<?php get_header(); ?>
+
+<div class="content">Content</div>
+
+<?php get_footer(); ?>
